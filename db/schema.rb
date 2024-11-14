95e6d89ff7daec88fe03326043127caf2953de83
Anurag Phalke <anurag.phalke@gmail.com>
2024-11-14 12:29:36 +0100

    Step 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_14_093855) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_14_112608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
     t.text "comment"
-    t.bigint "movie_id"
-    t.bigint "list_id"
+    t.bigint "list_id", null: false
+    t.bigint "movie_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
@@ -25,16 +25,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_14_093855) do
   end
 
   create_table "lists", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
-    t.text "title"
-    t.text "overview"
-    t.text "poster_url"
-    t.decimal "rating"
+    t.string "title"
+    t.string "overview"
+    t.string "poster_url"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
