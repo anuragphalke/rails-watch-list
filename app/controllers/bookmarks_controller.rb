8dc26d4ebe9413a9637357e2bb575bd6d86bb4e5
Anurag Phalke <anurag.phalke@gmail.com>
2024-11-14 15:27:05 +0100

    Step 4

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,34 +1,29 @@
 class BookmarksController < ApplicationController
   before_action :set_list
-  before_action :set_movie, only: [:create, :destroy]
 
   def create
-    @bookmark = @list.bookmarks.new(movie: @movie, comment: bookmark_params[:comment])
+    @bookmark = @list.bookmarks.new(bookmark_params)
 
     if @bookmark.save
-      redirect_to @list, notice: 'Movie bookmarked!'
+      redirect_to @list, notice: 'Movie added!'
     else
-      redirect_to @list, alert: 'Failed to bookmark movie.'
+      redirect_to @list, alert: "Failed to add the movie. #{@bookmark.errors.full_messages.join(', ')}"
     end
   end
 
   def destroy
-    @bookmark = @list.bookmarks.find(params[:id])
+    @bookmark = @list.bookmarks.find(params[:id])  # Find the bookmark by its ID in the list
     @bookmark.destroy
-    redirect_to @list, notice: 'Bookmark deleted.'
+    redirect_to @list, notice: 'Bookmark removed.'
   end
 
   private
 
   def set_list
-    @list = List.find(params[:list_id])
-  end
-
-  def set_movie
-    @movie = Movie.find(params[:movie_id])
+    @list = List.find(params[:list_id])  # Find the list by the list_id param
   end
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,23 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :destroy]
+  before_action :set_list, only: [:show]
 
   def index
     @lists = List.all
   end
 
   def show
+    @bookmark = Bookmark.new
+    @movies = Movie.all
   end
 
   def new
     @list = List.new
   end
+
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list, notice: "List created."
+      redirect_to @list, notice: 'List created.'
     else
       render :new
     end
